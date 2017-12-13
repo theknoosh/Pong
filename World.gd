b01037d77dbb67ce31b5a7579f87e90044d2ef8d
Darrell Payne <darrellpayne@earthlink.net>
2017-12-13 15:08:10 -0800

    Completed game

--- World.gd
+++ World.gd
@@ -5,17 +5,29 @@ const INTBALLSPEED = 100
 
 var screenSize
 var padSize
-var ballDirection = Vector2(1.0, 0.0)
+var ballDirection = Vector2(-1.0, 0.0)
 var ballSpeed = INTBALLSPEED
+var rightScore = 0
+var leftScore = 0
 
 func _ready():
+	
 	screenSize = get_viewport_rect().size
+	padSize = get_node("rightPlayer").get_texture().get_size()
+	
 	set_process(true)
 
 	pass
 	
 func _process(delta):
 
+	# Ball position
+	var ballPosition = get_node("ball").get_pos()
+	
+	#Colliders
+	var leftCollider = Rect2(get_node("LeftPlayer").get_pos() - padSize * 0.5, padSize)
+	var rightCollider = Rect2(get_node("rightPlayer").get_pos() - padSize * 0.5, padSize)
+	
 	var rightPlayerPosition = get_node("rightPlayer").get_pos()
 	var leftPlayerPosition = get_node("LeftPlayer").get_pos()
 
@@ -52,6 +64,36 @@ func _process(delta):
 	if (leftPlayerPosition.y > screenSize.height):
 		var resetPos = Vector2(leftPlayerPosition.x, -14.0)
 		get_node("LeftPlayer").set_pos(resetPos)
+		
+	ballPosition += ballDirection * ballSpeed * delta
+	if ((ballPosition                                                                                                                                   .y < 0 and ballDirection.y < 0) or (ballPosition.y > screenSize.height and ballDirection.y > 0)):
+		ballDirection.y = -ballDirection.y
+		
+	if (leftCollider.has_point(ballPosition) or rightCollider.has_point(ballPosition)):
+		ballDirection.x = -ballDirection.x
+		ballDirection.y = randf()*2 -1
+		ballDirection.normalized()
+		if(ballSpeed < 300):
+			ballSpeed *= 1.4
+			
+	#Game Over!
+	if (ballPosition.x < 0):
+		ballPosition = screenSize * 0.5
+		ballSpeed = INTBALLSPEED
+		rightScore += 1
+		ballDirection.x = -ballDirection.x
+		ballDirection.y = 0
+		
+	if (ballPosition.x > screenSize.width):
+		ballPosition = screenSize * 0.5
+		ballSpeed = INTBALLSPEED
+		leftScore += 1
+		ballDirection.x = - ballDirection.x
+		ballDirection.y = 0
+	
+	get_node("ball").set_pos(ballPosition)
+	get_node("leftScore").set_text(str(leftScore))
+	get_node("rightScore").set_text(str(rightScore))
 
 	
 		
